0a16f194ed78810adedc5f55aeb76538c4d4c298
Annie Flippo <acflippo@users.noreply.github.com>
2020-07-10 18:05:26 -0700

    Introduction to Computer Vision in Python
    Lesson 1

---
+++ Lesson1/intro_cv_lesson.py
@@ -0,0 +1,73 @@
+# example from https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+import cv2
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--first", required=True,
+	help="first input image")
+ap.add_argument("-s", "--second", required=True,
+	help="second")
+args = vars(ap.parse_args())
+
+# load the two input images
+imageA = cv2.imread(args["first"])
+imageB = cv2.imread(args["second"])
+
+# show the first image
+cv2.imshow("imageA", imageA)
+cv2.waitKey(0)
+
+# show the second image
+cv2.imshow("imageB", imageB)
+cv2.waitKey(0)
+
+# convert the images to grayscale
+grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+
+# show the greyed first image
+cv2.imshow("Grey imageA", grayA)
+cv2.waitKey(0)
+
+# show the greyed second image
+cv2.imshow("imageB", grayB)
+cv2.waitKey(0)
+
+# compute the Structural Similarity Index (SSIM) between the two
+# images, ensuring that the difference image is returned
+(score, diff) = compare_ssim(grayA, grayB, full=True)
+diff = (diff * 255).astype("uint8")
+print("SSIM: {}".format(score))
+
+# threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(diff, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# show the greyed second image
+cv2.imshow("Threshold", thresh)
+cv2.waitKey(0)
+
+
+# loop over the contours
+for c in cnts:
+	# compute the bounding box of the contour and then draw the
+	# bounding box on both input images to represent where the two
+	# images differ
+	(x, y, w, h) = cv2.boundingRect(c)
+	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+# show the output images
+cv2.imshow("Original", imageA)
+cv2.imshow("Modified", imageB)
+cv2.imshow("Diff", diff)
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
